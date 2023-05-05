bc52921e351b2fcb4b924ef784f24ae3ff8dc6bd
Ricekeet <ninjabasketball@hotmail.com>
2023-05-05 05:24:10 -0400

    updated balance to include optional mentionable

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -5,19 +5,28 @@ const economyHandler = require("../../methods/economyHandler");
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("balance")
-    .setDescription("Find out what your rice points balance is"),
+    .setDescription("Find out what your rice points balance is")
+    .addMentionableOption(option => option
+        .setName("user")
+        .setDescription("The user you want to check the balance of")
+        .setRequired(false)
+        )
+    ,
 
     async execute(interaction, client){
+        // interaction.options.get("user") should return a GuildMember object
+        const mentionedUser = interaction.options.get("user") ? interaction.options.get("user") : null;
         const numberWithCommas = (number) => {
             // Convert the number to a string and use a regular expression to add commas every 3rd digit
             return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
           }
-        const user = interaction.member;
-        let econUser = economyHandler.findUser(user.id);
+
+        const user = mentionedUser ? mentionedUser : interaction.member;
+        let econUser = economyHandler.findUser(user.user.id);
 
         let embed = new EmbedBuilder()
         .setTitle(`${user.nickname? user.nickname:user.user.username}'s balance`)
-        .setDescription(`You have ${numberWithCommas(econUser.balance)} rice points!`);
+        .setDescription(`${mentionedUser ? (user.nickname ? user.nickname : user.user.username):'You'} ${mentionedUser ? 'has':'have'} ${numberWithCommas(econUser.balance)} rice points!`);
 
         await interaction.reply({embeds: [embed]});
     }
