b32b9a8536a9980b33c4aceeb97622d81c7fb469
Ricekeet <ninjabasketball@hotmail.com>
2023-06-30 18:44:19 -0400

    fixed monthly

--- commands/economy/monthly.js
+++ commands/economy/monthly.js
@@ -24,14 +24,16 @@ module.exports = {
 
             econUser = econHandler.economy.economyData.find(x => x.id == user.id);
             if (econUser) {
+                console.log(`${Date.parse(econUser.lastMonthly)} ${Date.parse(econUser.lastMonthly) + 2629800000}`);
+                const nextMonthly =  new Date(Date.parse(econUser.lastMonthly) + 2629800000);
                 // If there is no record of monthly date, OR if the lastMonthly date IS MORE THAN lastMonthly + a month in miliseconds
-                if (econUser.lastMonthly == null || Date.parse(econUser.lastMonthly) >= Date.parse(econUser.lastMonthly) + 2629800000) {
+                if (econUser.lastMonthly == null || Date.now() >= nextMonthly) {
                     econUser.lastMonthly = today;
                     econHandler.addPoints(econUser.id, MONTHLY_POINTS);
                     console.log(`monthly: ${econUser.id} now has ${econUser.balance}`);
                     await interaction.reply({ content: `You claimed your monthly ${MONTHLY_POINTS.toLocaleString("en-US")} points! Run this command again next month for ${MONTHLY_POINTS.toLocaleString("en-US")} more!` });
                 } else {
-                    await interaction.reply(`You have already claimed your monthly ${MONTHLY_POINTS.toLocaleString("en-US")} this month! Try again next month`);
+                    await interaction.reply(`You have already claimed your monthly ${MONTHLY_POINTS.toLocaleString("en-US")} this month! Try again in **${nextMonthly.toDateString()}**`);
                 }
             }
             else {
